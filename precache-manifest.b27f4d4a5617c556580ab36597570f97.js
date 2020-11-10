a046d1fe46c299ecba9481994ee5af9c24931061
邓警方 <44660204+dengjingfang111@users.noreply.github.com>
2020-11-10 17:38:11 +0800

    任务五

---
+++ precache-manifest.b27f4d4a5617c556580ab36597570f97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9193d8a6df9711379006fef20cf8ddd1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18caaa14f0b13225e551",
+    "url": "/static/js/2.28811c00.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.28811c00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06496d014fd92ae35c06",
+    "url": "/static/js/main.f3df2038.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
